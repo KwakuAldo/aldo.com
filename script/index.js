55573854b81d6822207613ceb7786570a434f973
KwakuAldo <x.pac20032001@gmail.com>
2022-12-12 22:38:57 +0000

    Added the slide show and changed some HTML elements

---
+++ script/index.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex)
+
+function fwdSlides(n) {
+    showSlides(slideIndex += n)
+    console.log("clicked slide")
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('projectIMGs')
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    slides[slideIndex-1].style.display = "block"
+}
